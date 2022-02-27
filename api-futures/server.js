7600756c4e92f57acb7c0d8f8175c73df6f16233
Moncef-Tighi <monceftighiouart@gmail.com>
2022-02-27 12:14:20 +0100

    Lancement du serveur avec un exécutable

--- api-futures/server.js
+++ api-futures/server.js
@@ -5,8 +5,9 @@ const app = express();
 app.get("/fortunes", (request, response)=> {
     response.send("requesting");
 })
-
 const port = 3000
+
 app.listen(port, ()=> {
     console.log(`Server running on port ${port}`)
 });
+export default app;
