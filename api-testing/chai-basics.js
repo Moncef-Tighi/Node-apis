1bd909e9778d8c59f508beed59dbc3c202b24d96
Moncef-Tighi <monceftighiouart@gmail.com>
2022-03-11 14:22:48 +0100

    Assertion avec Chai

---
+++ api-testing/chai-basics.js
@@ -0,0 +1,23 @@
+const chai = require('chai');
+const expect = chai.expect;
+
+describe("File name", ()=> {
+    it("Should compare values", ()=>{
+        expect(1).to.equal(1);
+    })
+    it('Should fail', ()=> {
+        expect({name:"nom"}).to.equal({name: 'nom'});
+    })
+    it('Should sucessede', ()=> {
+        expect({name:"nom"}).to.deep.equal({name: 'nom'});
+        expect({name: 'nom'}).to.have.property('name').to.equal('nom');
+        expect(5>8).to.be.false;
+        expect({name: 'nom'}).to.be.a('object');
+        expect({name: 'nom'}.name).to.be.a('string')
+        expect([1,2,3].length).to.equal(3);
+        expect(null).to.be.null;
+        expect(undefined).to.not.exist;
+        expect(1).to.exist;
+    })
+
+}) 
